7ad1f53276b937148df8569d3aad19f1c2cf663c
Mathew Jacob <pmathewjacob@gmail.com>
2018-03-31 11:55:44 +0530

    adding speaker info and modifying logo

--- app/src/main/java/com/mtc/app/dyc/krp/AdminActivity.java
+++ app/src/main/java/com/mtc/app/dyc/krp/AdminActivity.java
@@ -1,11 +1,14 @@
 package com.mtc.app.dyc.krp;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,68 +32,119 @@ import java.util.List;
 
 public class AdminActivity extends BaseActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     private DatabaseReference mDatabase;
-    private FirebaseAuth mAuth;
+    private Button mExport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity);
 
-        Button export = findViewById(R.id.export);
+        mExport = findViewById(R.id.export);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
-        export.setOnClickListener(view -> {
-            mDatabase.child("adminRegister")
-                    .orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
 
-                    Gson gson = new Gson();
-                    String snap = gson.toJson(dataSnapshot.getValue(false), Object.class);
-                    showProgressDialog();
+        checkWritePermission();
 
-                    try {
+    }
 
-                        JSONObject json = new JSONObject(snap);
+    private void checkWritePermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-                        String csv = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/Users.csv");
-                        CSVWriter writer = new CSVWriter(new FileWriter(csv));
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
 
-                        List<String[]> data = new ArrayList<>();
-                        Iterator<String> iter = json.keys();
-                        while (iter.hasNext()) {
-                            String key = iter.next();
-                            JSONObject value = (JSONObject) json.get(key);
-                            Iterator<String> iterMini = value.keys();
-                            String val[] = new String[6];
-                            int x = 0;
-                            while (iterMini.hasNext()) {
-                                String temp = iterMini.next();
-                                val[x++] = value.getString(temp);
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mExport.setOnClickListener(view -> {
+                        mDatabase.child("adminRegister")
+                                .orderByValue().addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                Gson gson = new Gson();
+                                String snap = gson.toJson(dataSnapshot.getValue(false), Object.class);
+                                showProgressDialog();
+
+                                try {
+
+                                    JSONObject json = new JSONObject(snap);
+
+                                    String csv = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/Users.csv");
+                                    CSVWriter writer = new CSVWriter(new FileWriter(csv));
+
+                                    List<String[]> data = new ArrayList<>();
+                                    Iterator<String> iter = json.keys();
+                                    while (iter.hasNext()) {
+                                        String key = iter.next();
+                                        JSONObject value = (JSONObject) json.get(key);
+                                        Iterator<String> iterMini = value.keys();
+                                        String val[] = new String[6];
+                                        int x = 0;
+                                        while (iterMini.hasNext()) {
+                                            String temp = iterMini.next();
+                                            val[x++] = value.getString(temp);
+                                        }
+                                        data.add(val);
+
+                                        Log.d(AdminActivity.class.getName(), val + "");
+                                    }
+
+                                    writer.writeAll(data); // data is adding to csv
+                                    writer.close();
+
+                                } catch (JSONException | IOException e) {
+                                    e.printStackTrace();
+                                }
+                                hideProgressDialog();
+                                Log.d(AdminActivity.class.getName(), snap);
+                                Toast.makeText(getApplicationContext(), "Exported!", Toast.LENGTH_SHORT).show();
                             }
-                            data.add(val);
 
-                            Log.d(AdminActivity.class.getName(), val + "");
-                        }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
 
-                        writer.writeAll(data); // data is adding to csv
-                        writer.close();
+                            }
+                        });
 
-                    } catch (JSONException | IOException e) {
-                        e.printStackTrace();
-                    }
-                    hideProgressDialog();
-                    Log.d(AdminActivity.class.getName(), snap);
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Write Permission denied", Toast.LENGTH_LONG).show();
                 }
+                return;
+            }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-        });
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
